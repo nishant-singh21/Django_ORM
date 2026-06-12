2de9465cff5841011489af60e2ba0e668b585761
nishant-singh21 <nishantsingh1721@gmail.com>
2026-06-12 15:01:31 +0530

    changes

--- project13/blog/models.py
+++ project13/blog/models.py
@@ -4,7 +4,7 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField()
-    enrollment_date = models.DateField(auto_now_add=True)
+    city = models.CharField(max_length=100, default='Unknown')
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
